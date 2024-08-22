e4d8f17472eb4020eddee7810aa87fc2f5f8a184
Yoo Hyunseok <brianyhs@naver.com>
2024-08-22 11:37:50 +0900

    feat: board query, mutation frame

--- src/apis/boards/boards.service.ts
+++ src/apis/boards/boards.service.ts
@@ -1,11 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './entities/board.entity';
 
 // injection scopes : Singleton (new only once) (default option)
 //                    Request scope (new on every request)
 //                    Transient scope (new on each injection)
 @Injectable()
 export class BoardsService {
-  getBoards(): string {
-    return 'Hello World!';
+  findAll(): Board[] {
+    // 1. assuming we accessed DB & retrieved data
+    const result = [
+      {
+        number: 1,
+        writer: 'ryan',
+        title: 'hello there?',
+        contents: 'this is my content.',
+      },
+      {
+        number: 2,
+        writer: 'young',
+        title: 'keep',
+        contents: 'keep going',
+      },
+      {
+        number: 3,
+        writer: 'brian',
+        title: 'macbook for sale',
+        contents: 'dm for details',
+      },
+    ];
+
+    // 2. return fetched data from DB to browser as response
+    return result;
+  }
+
+  create(writer: string, title: string, contents: string): string {
+    // 1. logging data sent from browser
+    console.log(writer);
+    console.log(title);
+    console.log(contents);
+
+    // 2. assuming we accessed DB & saved data
+
+    // 3. return saved info to browser as response
+    return 'successfully created article.';
   }
 }
--- src/apis/boards/entities/board.entity.ts
+++ src/apis/boards/entities/board.entity.ts
@@ -1,16 +1,22 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity()
+@Entity() // typeorm decorator for database
+@ObjectType() // graphql decorator to mark this object as GraphQL type
 export class Board {
   @PrimaryGeneratedColumn('increment')
+  @Field(() => Int) // specifies GraphQL type
   number: number;
 
   @Column()
+  @Field(() => String)
   writer: string;
 
   @Column()
+  @Field(() => String)
   title: string;
 
   @Column()
+  @Field(() => String)
   contents: string;
 }
