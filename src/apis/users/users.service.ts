d003ffcdae8b331fbf6e3a139b8da7750c85cfd0
Yoo Hyunseok <brianyhs@naver.com>
2024-09-26 21:40:09 +0900

    feat: fix email validation on line 28, implement hashedPassword

--- src/apis/users/users.service.ts
+++ src/apis/users/users.service.ts
@@ -6,6 +6,7 @@ import {
   IUsersServiceCreate,
   IUsersServiceFindOneByEmail,
 } from './interfaces/users-service.interface';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -24,12 +25,15 @@ export class UsersService {
     name,
     age,
   }: IUsersServiceCreate): Promise<User> {
-    const user = this.findOneByEmail({ email });
+    const user = await this.findOneByEmail({ email });
     if (user) throw new ConflictException('Account already registered');
+
+    // cleaner code: hash service를 따로 만들어 비밀번호 생성 로직을 별도로 분리
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     return this.usersRepository.save({
       email,
-      password,
+      password: hashedPassword,
       name,
       age,
       // name: name, age: age ... 처럼 해야하지만 key-value가 동일한 문자열이므로 shorthand property
